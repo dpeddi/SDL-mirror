e16b3d6ecfb6165b671d02de72325259c75c4a62
Emanuel <emanuel@ihad.tv>
2017-03-30 12:19:04 +0200

    testgles2: add SDL_VIDEO_DRIVER_DREAMBOX

--- test/testgles2.c
+++ test/testgles2.c
@@ -48,6 +48,8 @@ static int LoadContext(GLES2_Context * data)
 #define __SDL_NOGETPROCADDR__
 #elif SDL_VIDEO_DRIVER_ANDROID
 #define __SDL_NOGETPROCADDR__
+#elif SDL_VIDEO_DRIVER_DREAMBOX
+#define __SDL_NOGETPROCADDR__
 #elif SDL_VIDEO_DRIVER_PANDORA
 #define __SDL_NOGETPROCADDR__
 #endif
